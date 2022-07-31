77a2bc9fd7af73348dde60d6ed41ecf20dc826be
tinatatana <109757952+tinatatana@users.noreply.github.com>
2022-07-31 18:58:48 +0200

    added humidity, wind speed

--- script.js
+++ script.js
@@ -43,6 +43,10 @@ function displayWeather(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 }
 
 function handleSubmit(event) {
