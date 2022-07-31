22276f50265404fdf707e57b803f9f4417035d81
tinatatana <109757952+tinatatana@users.noreply.github.com>
2022-07-31 21:29:07 +0200

    updated

--- script.js
+++ script.js
@@ -46,7 +46,14 @@ function displayWeather(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function handleSubmit(event) {
